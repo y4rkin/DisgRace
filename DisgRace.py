ff2ee9a3d191bf03bdbfc78d29d9a6d9f4b0985d
y4rkin <69445652+y4rkin@users.noreply.github.com>
2020-08-10 13:33:30 +0300

    Update DisgRace.py
    Added text centering instead of constant coordinates.

--- DisgRace.py
+++ DisgRace.py
@@ -8,14 +8,14 @@ from random import randint
 from time import sleep
 
 __author__ = "Berdar Yarkın Yücesoy"
-__version__= "0.1"
+__version__ = "0.1"
 
-__maintainer__= "Berdar Yarkın Yücesoy"
-__email__= "yarkinyucesoy@gmail.com"
-__status__= "Development"
+__maintainer__ = "Berdar Yarkın Yücesoy"
+__email__ = "yarkinyucesoy@gmail.com"
+__status__ = "Development"
 
-display_width = 1500
-display_height = 750
+DISPLAY_WIDTH = 1500
+DISPLAY_HEIGHT = 750
 
 car_width = 57
 car_height = 150
@@ -24,7 +24,7 @@ black = (0, 0, 0)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
-blue2 = (25,25,112)
+blue2 = (25, 25, 112)
 white = (255, 255, 255)
 white2 = (238, 238, 238)
 
@@ -40,104 +40,117 @@ pygame.mixer.init()
 pygame.mixer.music.load(r'.\music\Retro_Platforming_-_David_Fesliyan.mp3')
 pygame.mixer.music.play(-1)
 
-font = pygame.font.Font(r".\fonts\defused.ttf", 80)
+font1 = pygame.font.Font(r".\fonts\defused.ttf", 80)
 font2 = pygame.font.Font(r".\fonts\LucidaTypewriterRegular.ttf", 50)
 font3 = pygame.font.Font(r".\fonts\LucidaTypewriterRegular.ttf", 30)
 
-pautext = font.render("PAUSED",True, black)
+pautext = font1.render("PAUSED", True, black)
+pautext_rect = pautext.get_rect(center=(DISPLAY_WIDTH/2 , DISPLAY_HEIGHT/2 - 100))
 
-mainDisplay = pygame.display.set_mode((display_width, display_height))
+mainDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pygame.display.set_caption("DisgRace  v0.1")
 pygame.display.set_icon(icon)
 
 clock = pygame.time.Clock()
 
-def car(a,b):   #blits car
-    mainDisplay.blit(greenCar,(a, b))
 
-def game_exit(): 	#exits pygame and python both
+def car(a, b):  # blits car
+    mainDisplay.blit(greenCar, (a, b))
+
+
+def game_exit():  # exits pygame and python both
     pygame.quit()
-    print('\033[93m' + "-" * 30 + "\n          GAME OVER\n" + "-" * 30 + "\nScore: " + str(int(score)) + "   High Score:  " + str(int(hc_func(score))) + '\033[0m')
+    print('\033[93m' + "-" * 30 + "\n          GAME OVER\n" + "-" * 30 + "\nScore: " + str(
+        int(score)) + "   High Score:  " + str(int(hc_func(score))) + '\033[0m')
     sleep(3)
     quit()
+
 
 def game_over():
     go_message()
     game_loop()
 
-def go_message():	#shows game over message including score and high score
-    text = font.render("GAME OVER", True, black)
-    scotext = font.render("SCORE: {}".format(int(score)), True, black)
+
+def go_message():  # shows game over message including score and high score
+    text = font1.render("GAME OVER", True, black)
+    text_rect = text.get_rect(center=(DISPLAY_WIDTH/2, 95))
+
+    scotext = font1.render("SCORE: {}".format(int(score)), True, black)
     start3 = font2.render("Starting in 3", True, black)
     start2 = font2.render("Starting in 2", True, black)
     start1 = font2.render("Starting in 1", True, black)
+    start_rect = start1.get_rect(center=(DISPLAY_WIDTH/2 , DISPLAY_HEIGHT/2))
 
-    mainDisplay.blit(text, (450,50))
+    mainDisplay.blit(text, text_rect)
     mainDisplay.blit(scotext, (450, 150))
-    mainDisplay.blit(start3, (560, 350))
+    mainDisplay.blit(start3, start_rect)
     pygame.display.update()
     pygame.time.delay(1000)
     mainDisplay.fill(white2)
     show_score(score)
 
-    mainDisplay.blit(text, (450, 50))
+    mainDisplay.blit(text, text_rect)
     mainDisplay.blit(scotext, (450, 150))
-    mainDisplay.blit(start2, (560, 350))
+    mainDisplay.blit(start2, start_rect)
     pygame.display.update()
     pygame.time.delay(1000)
     mainDisplay.fill(white2)
     show_score(score)
 
-    mainDisplay.blit(text, (450, 50))
+    mainDisplay.blit(text, text_rect)
     mainDisplay.blit(scotext, (450, 150))
-    mainDisplay.blit(start1, (560, 350))
+    mainDisplay.blit(start1, start_rect)
     pygame.display.update()
     pygame.time.delay(1000)
 
-def pause():
-    mainDisplay.blit(pautext, (480, 250))
+
+def pause():    #Will have a quit button
+    mainDisplay.blit(pautext, pautext_rect)
     pygame.display.update()
+
 
 def hc_func(new_score):
     global high_score
-    if new_score>high_score:
+    if new_score > high_score:
         high_score = new_score
     return high_score
 
+
 def show_score(sc):
     sc_widget = font3.render("Score: " + str(int(sc)), True, black)
-    mainDisplay.blit(sc_widget,(0,0))
+    mainDisplay.blit(sc_widget, (0, 0))
 
-def draw_rect(objlist,speed):	#creates, moves and draws rectangle obstacles
+
+def draw_rect(objlist, speed):  # creates, moves and draws rectangle obstacles
     if len(objlist) < 2:
         objlist.append(Rectangle())
     for object in objlist:
         object.y += speed
         pygame.draw.rect(mainDisplay, blue2, (object.x, object.y, object.width, object.height))
-        if object.y> display_height:
+        if object.y > DISPLAY_HEIGHT:
             objlist.remove(object)
 
-def coll(o_list,car_x,car_y):  #checks for collusions
+
+def coll(o_list, car_x, car_y):  # checks for collusions
     x = car_x
     y = car_y
-    for object in o_list:
-        if (y + car_height)>object.y>(y - object.height):
+    for obj in o_list:
+        if (y + car_height) > obj.y > (y - obj.height):
             print("y crossover")
-            if (x+car_width)>object.x>(x-object.width):
+            if (x + car_width) > obj.x > (x - obj.width):
                 print("x crossover")
                 game_over()
+
 
 class Rectangle:
     def __init__(self):
         self.height = 150
         self.width = randint(200, 400)
-        self.x = randint(-self.width, display_width)
+        self.x = randint(-self.width, DISPLAY_WIDTH)
         self.y = -150
 
 
-
-
-def game_loop():		#main loop
+def game_loop():  # main loop
     rect1 = Rectangle()
     rect1.y = -150
     rect2 = Rectangle()
@@ -149,8 +162,8 @@ def game_loop():		#main loop
 
     objs = [rect1, rect2]
 
-    x = display_width / 2
-    y = display_height - car_height - 10
+    x = DISPLAY_WIDTH / 2
+    y = DISPLAY_HEIGHT - car_height - 10
 
     left_hold = False
     right_hold = False
@@ -161,7 +174,7 @@ def game_loop():		#main loop
     s = 10
     global score
     score = .0
-	
+
     while running:
         for event in pygame.event.get():
             print(event)
@@ -180,7 +193,7 @@ def game_loop():		#main loop
                 if event.key == pygame.K_RIGHT:
                     right_hold = False
 
-        if paused:
+        if paused:    
             pause()
             continue
 
@@ -189,26 +202,29 @@ def game_loop():		#main loop
         if right_hold:
             x += n
 
-        if x>display_width-car_width:
-            x = display_width-car_width
-        elif x<0:
+        if x > DISPLAY_WIDTH - car_width:
+            x = DISPLAY_WIDTH - car_width
+        elif x < 0:
             x = 0
 
         n += 0.002
         s += 0.002
-        score += s/60
+        score += s / 60
         hc_func(score)
 
         mainDisplay.fill(white2)
         show_score(score)
-        draw_rect(objs,s)
-        car(x,y)
-        coll(objs,x,y)
+        draw_rect(objs, s)
+        car(x, y)
+        coll(objs, x, y)
 
         pygame.display.update()
         clock.tick(60)
     game_exit()
 
-if __name__ == '__main__':	
-	game_loop()
-	game_exit()
+def main():       #Will have features like: quit, restart, change language, fullscreen mode...
+    game_loop()
+    game_exit()
+
+if __name__ == '__main__':
+    main()
